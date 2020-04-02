f786a7f8e391541ce87fe2858089b51e6b67a8ed
libou <libou0924@163.com>
2020-04-02 15:29:02 -0700

    implement popularity_scraping.py

--- WebScraping/popularity_scraping.py
+++ WebScraping/popularity_scraping.py
@@ -4,40 +4,85 @@ Crawl the popularity of a specific movie on Twitter from www.trendsmap.com
 import json
 import requests
 import pandas as pd
+from WebScraping.proxy.proxy_pool import Proxy
+import datetime
+import re
 
 
-def popularity_scrap(name, proxy):
-    https_url = "https://www.trendsmap.com/api/keyword_volumes/%23{}".format(name)
-    headers = {
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-        'path': "/api/keyword_volumes/%23{}".format(name),
-        'referer': "https://www.trendsmap.com/historical?q={}".format(name), 'authority': "www.trendsmap.com",
-        'accept': "application/json, text/javascript, */*; q=0.01", 'accept-encoding': "gzip, deflate, br",
-        'accept-language': "zh-CN,zh;q=0.9", 'origin': "https://www.trendsmap.com", 'sec-fetch-mode': "cors",
-        'sec-fetch-site': "same-origin", 'x-requested-with': "XMLHttpRequest"}
-    try:
-        # data = {}
-        # data = json.dumps(data)
-        proxies = {"https": proxy}
-        res = requests.post(https_url, headers=headers, timeout=10, proxies=proxies)
-        content = res.content.decode("utf-8")
+def popularity_scrap(names, date, proxy):
+    """
+    Crawl the popularity of a movie discussed on Twitter
+    :param name: a list of extracted keywords of the movie's title
+    :param date: the timestamp of the release date
+    :param proxy: the address of the proxy server
+    :return: a tuple including the average popularity from 2014 to 2020, the average popularity within 30
+    days around the release date, the max popularity within 30 days around the release date
+    """
 
-        data = json.loads(content)['data']
-        result = []
-        for d in data:
-            result.append([name, d[0], d[1][0]])
-        df = pd.DataFrame(result, columns=['name', 'date', 'count'])
-        df.to_csv('popularity.csv', mode='a')
+    avg_all = 0
+    avg_30 = 0
+    max_30 = 0
+    for name in names:
+        https_url = "https://www.trendsmap.com/api/keyword_volumes/%23{}".format(name)
+        headers = {
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+            'path': "/api/keyword_volumes/%23{}".format(name),
+            'referer': "https://www.trendsmap.com/historical?q={}".format(name), 'authority': "www.trendsmap.com",
+            'accept': "application/json, text/javascript, */*; q=0.01", 'accept-encoding': "gzip, deflate, br",
+            'accept-language': "zh-CN,zh;q=0.9", 'origin': "https://www.trendsmap.com", 'sec-fetch-mode': "cors",
+            'sec-fetch-site': "same-origin", 'x-requested-with': "XMLHttpRequest"}
+        try:
+            # data = {}
+            # data = json.dumps(data)
+            proxies = {"http": proxy}
+            res = requests.post(https_url, headers=headers, proxies=proxies)
+            if res.status_code != 200:
+                raise Exception
 
-        if res.status_code == 200:
-            return True
-        return False
-    except Exception as e:
-        msg = str(e)
-        print(msg)
-        return False
+            content = res.content.decode("utf-8")
+            data = json.loads(content)['data']
+            result = []
+            for d in data:
+                # d[0]:timestamp  d[1][0]: value
+                result.append([name, d[0], d[1][0]])
+            df = pd.DataFrame(result, columns=['name', 'time', 'popularity'])
+            if df['popularity'].mean() > avg_all:
+                avg_all = df['popularity'].mean()
+
+            left_date = datetime.datetime.fromtimestamp(date) - datetime.timedelta(days=15)
+            right_date = datetime.datetime.fromtimestamp(date) + datetime.timedelta(days=15)
+            df_30 = df.loc[(df['time'] >= left_date.timestamp()) & (df['time'] <= right_date.timestamp())]
+            if df_30['popularity'].mean() > avg_30:
+                avg_30 = df_30['popularity'].mean()
+            if df_30['popularity'].max() > max_30:
+                max_30 = df_30['popularity'].max()
+        except Exception as e:
+            msg = str(e)
+            print(msg)
+
+    return avg_all, avg_30, max_30
 
 
-df = pd.read_csv("IMDB_Top10000.csv", header=True)
-for idx, row in df['name'].iterrows():
-    print(row)
+inst_proxy = Proxy('proxy/proxy.txt')
+input_year = int(input("Please enter start year (eg. 2016): "))
+current_year = int(datetime.datetime.now().year)
+assert input_year <= current_year
+df = pd.read_csv("../data/IMDB_movie_detail_{}.csv".format(input_year))
+result = []
+for idx, row in df.iterrows():
+    movie = row['movie']
+    name = row['extracted_name']
+    date_str = row['release_date']
+    date = datetime.datetime.strptime(date_str, '%d-%m-%y').timestamp()
+    while True:
+        proxy = inst_proxy.get_proxy()
+        try:
+            (avg_all, avg_30, max_30) = popularity_scrap(name, date, proxy=proxy)
+            result.append([movie, avg_all, avg_30, max_30])
+            break
+        except Exception:
+            print("Wrong proxy: {}".format(proxy))
+            continue
+
+new_df = pd.DataFrame(result, columns=['movie', 'avg_all', 'avg_30', 'max_30'])
+new_df.to_csv('../data/movie_popularity_{}'.format(input_year), header=True, encoding='utf_8_sig')
--- WebScraping/proxy/proxy_test.py
+++ WebScraping/proxy/proxy_test.py
@@ -3,14 +3,22 @@ from WebScraping.proxy.proxy_pool import Proxy
 
 
 def test_proxy(proxy, https_url):
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+    # headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+        'path': "/api/keyword_volumes/%23avengersendgame",
+        'referer': "https://www.trendsmap.com/historical?q=avengersendgame", 'authority': "www.trendsmap.com",
+        'accept': "application/json, text/javascript, */*; q=0.01", 'accept-encoding': "gzip, deflate, br",
+        'accept-language': "zh-CN,zh;q=0.9", 'origin': "https://www.trendsmap.com", 'sec-fetch-mode': "cors",
+        'sec-fetch-site': "same-origin", 'x-requested-with': "XMLHttpRequest"}
     try:
-        proxies = {"https": proxy}
-        res = requests.get(https_url, headers=headers, proxies=proxies, timeout=10)
+        proxies = {"http": proxy}
+        res = requests.post(https_url, headers=headers, proxies=proxies)
         content = res.content.decode("utf-8")
 
         # print(res.status_code)
         # print(content)
+        print(res.status_code)
         if res.status_code == 200:
             return True
         return False
@@ -24,7 +32,7 @@ inst = Proxy('proxy.txt')
 count = 0
 for i in range(inst.get_proxies_num()):
     proxy = inst.get_proxy()
-    url = "https://www.imdb.com/search/title/?release_date=2019-01-01,2019-12-31&sort=num_votes,desc&ref_=adv_prv"
+    url = "https://www.trendsmap.com/api/keyword_volumes/%23avengersendgame"
     if not test_proxy(proxy, url):
         count += 1
         print("Wrong Proxy: {}".format(proxy))
