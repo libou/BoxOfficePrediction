f61e10f1a860eae1b62be7065e19a01adf81b59e
libou <libou0924@163.com>
2020-04-08 17:54:40 -0700

    add holiday feature

--- WebScraping/holiday_scraping.py
+++ WebScraping/holiday_scraping.py
@@ -1,13 +1,34 @@
 import pandas as pd
-from .proxy.proxy_pool import Proxy
+from WebScraping.proxy.proxy_pool import Proxy
 from bs4 import BeautifulSoup
 from requests import get
+from tqdm import tqdm
 
 inst_proxy = Proxy("proxy/proxy.txt")
 
-url = "https://www.timeanddate.com/holidays/us/2011?hol=25"
 headers = {'User-agent': 'Mozilla/5.0'}
 
+result = []
 years = range(2008, 2020)
+for year in tqdm(years):
+    url = "https://www.calendarlabs.com/holidays/us/{}".format(year)
+    proxy = inst_proxy.get_proxy()
+    proxies = {"http": proxy}
 
-res = get(url, headers=headers, )
+    res = get(url, headers=headers, proxies=proxies)
+    if res.status_code != 200:
+        raise Exception
+
+    html_soup = BeautifulSoup(res.text, 'html.parser')
+    holiday_containers = html_soup.tbody
+    holidays = holiday_containers.find_all('tr')
+
+    for holiday in holidays:
+        containers = holiday.find_all('td')
+        date = containers[1].span.text
+        weekday = containers[0].span.text
+        name = containers[2].text
+        result.append([date, weekday, name])
+
+df = pd.DataFrame(result, columns=['date', 'weekday', 'name'])
+df.to_csv('../data/holiday.csv', header=True, index=None)
